3884f760090b7550f4963920cffee9e4e2ef0b7c
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-4.local>
2023-12-03 09:46:53 -0800

    random user generator created

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const axios = require("axios");
 
+/**
+ * Returns a random element from the given array.
+ *
+ * @param {Array} arr - The array from which to retrieve a random element.
+ * @return {*} - A random element from the array.
+ */
 function getRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+/**
+ * Generates a random beer.
+ *
+ * @return {Object} - An object representing a random beer with the following properties:
+ *   - name: {string} - The name of the beer.
+ *   - tagline: {string} - A tagline for the beer.
+ *   - abv: {number} - The alcohol by volume (ABV) of the beer.
+ *   - ibu: {number} - The International Bitterness Units (IBU) of the beer.
+ *   - foodPairing: {Array} - An array of strings representing food pairings for the beer.
+ */
 function generateRandomBeer() {
   const beerNames = [
     "Hoppy Ale",
@@ -39,6 +56,12 @@ function generateRandomBeer() {
     ],
   };
 }
+
+/**
+ * Generates a random cocktail with a name, tagline, list of ingredients, and instructions.
+ *
+ * @return {object} An object containing the random cocktail details.
+ */
 
 function generateRandomCocktail() {
   const cocktailNames = [
@@ -77,7 +100,25 @@ function generateRandomCocktail() {
     instructions: instructions,
   };
 }
+/**
+ * Retrieves a random user from an external API.
+ *
+ * @return {Object} An object containing the full name of the user.
+ */
+const getRandomUser = async () => {
+  try {
+    const response = await axios.get("https://randomuser.me/api/");
 
+    const userData = response.data.results[0];
+    const fullName = `${userData.name.first} ${userData.name.last}`;
+    return {
+      fullName,
+    };
+  } catch (error) {
+    // Handle errors
+    throw new Error(`Error fetching random user: ${error.message}`);
+  }
+};
 app.get("/beer", (req, res) => {
   const randomBeer = generateRandomBeer();
   res.json(randomBeer);
@@ -86,6 +127,12 @@ app.get("/beer", (req, res) => {
 app.get("/cocktail", (req, res) => {
   const randomCocktail = generateRandomCocktail();
   res.json(randomCocktail);
+});
+
+app.get("/suggestion", async (req, res) => {
+  const user = await getRandomUser();
+  console.log(`Random user inside suggestion: ${user.fullName}`);
+  res.json(user);
 });
 
 app.listen(PORT, () => {
