8d11fc80604b14fe8ca86f08cfce83cc1f640baf
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-4.local>
2023-12-03 19:23:50 -0800

    everything working

--- index.js
+++ index.js
@@ -3,33 +3,17 @@ const app = express();
 const PORT = 3000;
 const axios = require("axios");
 const {
-  getRandomElement,
-  generateRandomBeer,
   isDaytime,
-  generateRandomCocktail,
   getRandomUser,
   getCityTimeZoneByName,
   getCurrentTimeInTimeZone,
   getIngredients,
+  fetchCocktail,
 } = require("./utils");
-
-// app.get("/beer", (req, res) => {
-//   const randomBeer = generateRandomBeer();
-//   res.json(randomBeer);
-// });
-
-// app.get("/cocktail", async (req, res) => {
-//   // const randomCocktail = generateRandomCocktail();
-//   const randomCocktail = await axios.get(
-//     "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-//   );
-//   res.json(randomCocktail.data);
-// });
 
 app.get("/beer", async (req, res) => {
   try {
     const response = await axios.get("https://api.punkapi.com/v2/beers/random");
-
     const beerData = response.data[0];
     const beerInfo = {
       name: beerData.name,
@@ -48,17 +32,9 @@ app.get("/beer", async (req, res) => {
 
 app.get("/cocktail", async (req, res) => {
   try {
-    const response = await axios.get(
+    const cocktailInfo = await fetchCocktail(
       "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     );
-
-    const cocktailData = response.data.drinks[0];
-    const cocktailInfo = {
-      name: cocktailData.strDrink,
-      tagline: cocktailData.strTags,
-      ingredients: getIngredients(cocktailData),
-      instructions: cocktailData.strInstructions,
-    };
     res.json(cocktailInfo);
   } catch (error) {
     console.error("Error fetching random cocktail:", error.message);
@@ -74,19 +50,59 @@ app.get("/suggestion", async (req, res) => {
     let currentTime = getCurrentTimeInTimeZone(response.data.timezone);
     currentTime = currentTime.toFormat("HH:mm");
     console.log(`Current time in ${user.city}:`, currentTime);
+    console.log("user name", user.fullName[0][0]);
+    let userName = user.fullName[0][0];
     let isDay = isDaytime(currentTime);
     let data;
     if (isDay) {
       const beerResponse = await axios.get("http://localhost:3000/beer");
       data = beerResponse.data;
     } else {
-      data = generateRandomCocktail();
+      data = await fetchCocktail(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${userName}`
+      );
     }
     res.json(data);
   } else {
     console.error(`Error: ${response.status}`, response.data);
   }
 });
+// app.get("/suggestion", async (req, res) => {
+//   try {
+//     const user = await getRandomUser();
+//     const response = await getCityTimeZoneByName(user.city);
+
+//     if (response.status !== 200) {
+//       console.error(`Error: ${response.status}`, response.data);
+//       return res.status(500).json({ error: "Internal server error" });
+//     }
+
+//     const currentTime = getCurrentTimeInTimeZone(
+//       response.data.timezone
+//     ).toFormat("HH:mm");
+//     // console.log(`Current time in ${user.city}:`, currentTime);
+//     // console.log("user name", user.fullName[0][0]);
+//     const isDay = isDaytime(currentTime);
+
+//     let [beerData, cocktailData] = await Promise.all([
+//       isDay ? axios.get("http://localhost:3000/beer") : null,
+//       !isDay
+//         ? fetchCocktail(
+//             "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+//           )
+//         : null,
+//     ]);
+
+//     // Handle null values in case one of the requests was skipped
+//     beerData = beerData ? beerData.data : null;
+//     cocktailData = cocktailData ? cocktailData : null;
+
+//     res.json({ beerData, cocktailData });
+//   } catch (error) {
+//     console.error("Error in /suggestion:", error.message);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- utils.js
+++ utils.js
@@ -3,87 +3,11 @@ const apiKey = "koDgoe9bc4Ffgvh8rgzWWQ==PC7lyNMTmcFAFoVa";
 const { DateTime } = require("luxon");
 const axios = require("axios");
 
-function getRandomElement(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-function generateRandomBeer() {
-  const beerNames = [
-    "Hoppy Ale",
-    "Golden Lager",
-    "Stout Delight",
-    "Amber Ale",
-    "Wheat Bliss",
-  ];
-  const taglines = [
-    "Brewed to Perfection",
-    "Crafted with Love",
-    "A Taste of Tradition",
-    "Bold and Balanced",
-  ];
-  const foodPairings = [
-    "Spicy Tacos",
-    "Cheese Platter",
-    "Grilled Salmon",
-    "Chocolate Cake",
-  ];
-
-  return {
-    name: getRandomElement(beerNames),
-    tagline: getRandomElement(taglines),
-    abv: parseFloat((Math.random() * (10 - 4) + 4).toFixed(1)),
-    ibu: Math.floor(Math.random() * (100 - 10 + 1) + 10),
-    foodPairing: [
-      getRandomElement(foodPairings),
-      getRandomElement(foodPairings),
-      getRandomElement(foodPairings),
-    ],
-  };
-}
-
 function isDaytime(time) {
   const hours = parseInt(time.split(":")[0]);
   const daytimeStart = 6;
   const daytimeEnd = 18;
   return hours >= daytimeStart && hours < daytimeEnd;
-}
-
-function generateRandomCocktail() {
-  const cocktailNames = [
-    "Mojito",
-    "Cosmopolitan",
-    "Old Fashioned",
-    "Martini",
-    "Pina Colada",
-  ];
-  const taglines = [
-    "Classic Elegance",
-    "Tropical Delight",
-    "Timeless Flavor",
-    "The Perfect Mix",
-  ];
-  const ingredients = [
-    "Vodka",
-    "Gin",
-    "Rum",
-    "Tequila",
-    "Triple Sec",
-    "Lime Juice",
-    "Simple Syrup",
-  ];
-  const instructions =
-    "Mix all ingredients in a shaker with ice and strain into a glass.";
-
-  return {
-    name: getRandomElement(cocktailNames),
-    tagline: getRandomElement(taglines),
-    ingredients: [
-      getRandomElement(ingredients),
-      getRandomElement(ingredients),
-      getRandomElement(ingredients),
-    ],
-    instructions: instructions,
-  };
 }
 
 async function getRandomUser() {
@@ -147,13 +71,30 @@ function getIngredients(cocktailData) {
   return ingredients;
 }
 
+async function fetchCocktail(apiUrl) {
+  try {
+    const response = await axios.get(apiUrl);
+
+    const cocktailData = response.data.drinks[0];
+    const cocktailInfo = {
+      name: cocktailData.strDrink,
+      tagline: cocktailData.strTags,
+      ingredients: getIngredients(cocktailData),
+      instructions: cocktailData.strInstructions,
+    };
+
+    return cocktailInfo;
+  } catch (error) {
+    console.error("Error fetching random cocktail:", error.message);
+    throw new Error("Internal server error");
+  }
+}
+
 module.exports = {
-  getRandomElement,
-  generateRandomBeer,
   isDaytime,
-  generateRandomCocktail,
   getRandomUser,
   getCityTimeZoneByName,
   getCurrentTimeInTimeZone,
   getIngredients,
+  fetchCocktail,
 };
